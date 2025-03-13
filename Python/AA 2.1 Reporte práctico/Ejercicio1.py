5901509feddea08fa9485ec64dac6afec912829a
Farid221102 <151697858+Farid221102@users.noreply.github.com>
2025-03-13 16:59:35 -0500

    Add files via upload

---
+++ Python/AA 2.1 Reporte práctico/Ejercicio1.py
@@ -0,0 +1,12 @@
+# Solicitar datos al usuario
+nombre = input("Ingrese su nombre completo: ")
+semestre = input("Ingrese su semestre: ")
+grupo = input("Ingrese su grupo: ")
+carrera = input("Ingrese su carrera: ")
+
+# Mostrar los datos ingresados
+print("\n--- Datos ingresados ---")
+print("Nombre completo:", nombre)
+print("Semestre:", semestre)
+print("Grupo:", grupo)
+print("Carrera:", carrera)
