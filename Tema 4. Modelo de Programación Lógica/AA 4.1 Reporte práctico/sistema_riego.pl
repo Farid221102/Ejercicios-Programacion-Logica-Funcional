33eaedecd6b20006d142575d5119c713861f7cdc
Farid221102 <151697858+Farid221102@users.noreply.github.com>
2025-06-02 02:48:42 -0500

    Add files via upload

---
+++ Tema 4. Modelo de Programación Lógica/AA 4.1 Reporte práctico/sistema_riego.pl
@@ -0,0 +1,39 @@
+﻿% Se tendrán las zonas norte, sur y centro
+% Hechos
+humedad(norte, baja).
+humedad(sur, baja).
+humedad(centro, baja).
+
+temperatura(norte, 34).
+temperatura(sur, 30).
+temperatura(centro, 36).
+
+pronostico_lluvia(norte, false).
+pronostico_lluvia(sur, false).
+pronostico_lluvia(centro, true).
+
+hora(19).
+
+% Reglas
+hora_adecuada :-   hora(H), (H < 10 ; H > 18).
+
+regar(Zona) :- 
+    humedad(Zona, baja),
+    pronostico_lluvia(Zona, false),
+    hora_adecuada.
+
+estado(Zona, 'Activado') :- regar(Zona), !.
+estado(Zona, 'No activado') :- \+ regar(Zona).
+
+alerta(Zona) :-    temperatura(Zona, T), T >= 32.
+
+recomendacion(Zona) :-
+    regar(Zona),
+    alerta(Zona), !,
+    writeln('ALERTA: Riego activado con temperatura alta. Considere regar de noche o por goteo').
+
+recomendacion(Zona) :-
+    \+ regar(Zona), !,
+    writeln('No es necesario activar el riego. Está lloviendo').
+
+recomendacion(Zona) :- writeln(['Sin recomendaciones especiales para el riego', Zona]).
